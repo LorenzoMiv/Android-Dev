ae5d8080a5a50b8a91d62bd58bcdb1521438aa0d
Lorenzo Murillo IV <Lorenzo.murillo01@student.csulb.edu>
2023-10-01 17:02:56 -0700

    Completed Code Lab 6 and removed redundant file

---
+++ Android Studio Labs/TipCalculatore/app/src/androidTest/java/com/example/tipcalculatore/TipUITests.kt
@@ -0,0 +1,32 @@
+package com.example.tipcalculatore
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tipcalculatore.ui.theme.TipCalculatoreTheme
+import org.junit.Rule
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipUITests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipCalculatoreTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule
+            .onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule
+            .onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists("No node with this text was found")
+    }
+
+}
